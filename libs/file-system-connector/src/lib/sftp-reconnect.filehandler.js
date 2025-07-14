d61d11c53cc7a3022e5665353456d84c3c1aa63c
Rene Heijdens <rene.heijdens@x-ip.nl>
2025-07-14 15:30:23 +0200

    v0.7.31

--- libs/file-system-connector/src/lib/sftp-reconnect.filehandler.js
+++ libs/file-system-connector/src/lib/sftp-reconnect.filehandler.js
@@ -23,6 +23,7 @@ module.exports = __toCommonJS(sftp_reconnect_filehandler_exports);
 var import_logger = require("@transai/logger");
 var import_sftp = require("./sftp.filehandler");
 class SftpReconnectFilehandler extends import_sftp.SftpFilehandler {
+  #logger;
   constructor(config) {
     if (config.type === "sftp") {
       throw new Error(
@@ -30,50 +31,83 @@ class SftpReconnectFilehandler extends import_sftp.SftpFilehandler {
       );
     }
     super(config);
-    import_logger.Logger.getInstance().info("SFTP Reconnect File handler setup!");
+    this.#logger = import_logger.Logger.getInstance();
+    this.#logger.info("SFTP Reconnect File handler setup!");
   }
   async init() {
     await super.init();
+    this.#logger.debug("SFTP Reconnect initialized! disconnecting client.");
     await this.sftpClient.end();
+    this.#logger.debug("SFTP Reconnect initialized! Client disconnected.");
   }
   async list(dir) {
+    this.#logger.debug(
+      `SFTP Reconnect listing directory: ${dir}. First connecting to SFTP server.`
+    );
     await this.sftpClient.connect(this.config);
+    this.#logger.debug("SFTP Reconnect connected to SFTP server.");
     try {
       return await super.list(dir);
     } finally {
+      this.#logger.debug("SFTP Reconnect disconnecting from SFTP server.");
       await this.sftpClient.end();
+      this.#logger.debug("SFTP Reconnect disconnected from SFTP server.");
     }
   }
   async readFile(filepath) {
+    this.#logger.debug(
+      `SFTP Reconnect reading file: ${filepath}. First connecting to SFTP server.`
+    );
     await this.sftpClient.connect(this.config);
+    this.#logger.debug("SFTP Reconnect connected to SFTP server.");
     try {
       return super.readFile(filepath);
     } finally {
+      this.#logger.debug("SFTP Reconnect disconnecting from SFTP server.");
       await this.sftpClient.end();
+      this.#logger.debug("SFTP Reconnect disconnected from SFTP server.");
     }
   }
   async writeFile(data, remotePath, filename) {
+    this.#logger.debug(
+      `SFTP Reconnect writing file: ${filename} to path: ${remotePath}. First connecting to SFTP server.`
+    );
     await this.sftpClient.connect(this.config);
+    this.#logger.debug("SFTP Reconnect connected to SFTP server.");
     try {
       return super.writeFile(data, remotePath, filename);
     } finally {
+      this.#logger.debug("SFTP Reconnect disconnecting from SFTP server.");
       await this.sftpClient.end();
+      this.#logger.debug("SFTP Reconnect disconnected from SFTP server.");
     }
   }
   async deleteFile(filepath) {
+    this.#logger.debug(
+      `SFTP Reconnect deleting file: ${filepath}. First connecting to SFTP server.`
+    );
     await this.sftpClient.connect(this.config);
+    this.#logger.debug("SFTP Reconnect connected to SFTP server.");
     try {
       return super.deleteFile(filepath);
     } finally {
+      this.#logger.debug("SFTP Reconnect disconnecting from SFTP server.");
       await this.sftpClient.end();
+      this.#logger.debug("SFTP Reconnect disconnected from SFTP server.");
     }
   }
   async fileExists(filepath) {
+    this.#logger.debug(
+      `SFTP Reconnect checking if file exists: ${filepath}. First connecting to SFTP server.`
+    );
     await this.sftpClient.connect(this.config);
+    this.#logger.debug("SFTP Reconnect connected to SFTP server.");
     try {
       return super.fileExists(filepath);
     } finally {
+      this.#logger.debug("SFTP Reconnect disconnecting from SFTP server.");
       await this.sftpClient.end();
+      this.#logger.debug("SFTP Reconnect disconnected from SFTP server.");
     }
   }
 }
